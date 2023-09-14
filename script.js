0754e28a7f657a19333c4549d66f38384c7ba1c5
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-09-15 01:54:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const bi = document.getElementById("browser-info");
+
+const browserName = navigator.appName;
+const browserName1 = navigator.appVersion;
+const browserName2 = navigator.userAgent;
+
+
+bi.innerHTML = `You are using ${browserName} version ${browserName1} `
